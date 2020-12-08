47ed92cc27e955a7bde98fb4fa21ce8a098ca4d3
musa-atlihan <msatlihan@gmail.com>
2020-12-08 23:17:07 +0300

    add etl queries

---
+++ python/airflow/dags/utils/etl/queries.py
@@ -0,0 +1,43 @@
+# create tables
+
+create_quake_tr_table = """
+CREATE TABLE IF NOT EXISTS quake_tr
+(
+    quake_id SERIAL PRIMARY KEY,
+    quake_code INT,
+    quake_time TIMESTAMP,
+    lat FLOAT,
+    lon FLOAT,
+    depth FLOAT,
+    xm FLOAT,
+    md FLOAT,
+    ml FLOAT,
+    mw FLOAT,
+    ms FLOAT,
+    mb FLOAT,
+    quake_type VARCHAR,
+    location VARCHAR
+);
+"""
+
+
+# insert queries
+
+insert_quake_tr_table = """
+INSERT INTO quake_tr
+(
+    quake_id, quake_code, quake_time,
+    lat, lon, depth,
+    xm, md, ml, 
+    mw, ms, mb, 
+    quake_type, location
+)
+VALUES
+(
+    DEFAULT, %s, %S,
+    %S, %S, %S,
+    %S, %S, %S,
+    %S, %S, %S,
+    %S, %S
+)
+"""
